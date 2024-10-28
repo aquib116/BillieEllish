735fae95ce9cbd99e2d1d233b37b64e55591766b
Ansusameer <ansusameer@outlook.com>
2024-10-28 23:26:46 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -61,11 +61,11 @@ Nᴀᴍᴇ - {}"""
 
     TOP_ALRT_MSG = """🔆彡[ @theAvaflix ]彡🔆"""
 
-    MELCOW_ENG = """<b><i>🔆 "theAvaflix • GROUP" 亗 🔆
+    MELCOW_ENG = """<b><i>🔆 "THE NARUTO MOVIE HUB • GROUP" 亗 🔆
 •───────────────────• 
-⚜Hey {}, Welcome To theAvaflix • Group. You Can Request Any Movies, Web-Series, Anime, K-Dramas, Animation etc., here....</i></b>"""
+⚜Hey {}, Welcome To THE NARUTO MOVIE HUB • Group. You Can Request Any Movies, Web-Series, Anime, K-Dramas, Animation etc., here....</i></b>"""
 
-    SHORTLINK_INFO = """🔆彡[ @theAvaflix ]彡🔆"""
+    SHORTLINK_INFO = """🔆彡[ @howuseme ]彡🔆"""
 
     REQINFO = """🔆彡[ @theAvaflix ]彡🔆"""
 
